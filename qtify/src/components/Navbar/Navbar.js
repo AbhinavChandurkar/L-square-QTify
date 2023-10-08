3390a23401bf401007e9dafe5ea66c335712f137
Abhinav Chandurkar <abhinavchandurkar55@gmail.com>
2023-10-08 08:17:14 +0530

    C4: Search Bar

--- qtify/src/components/Navbar/Navbar.js
+++ qtify/src/components/Navbar/Navbar.js
@@ -2,10 +2,13 @@ import React from "react";
 import Button from "../Button/Button";
 import style from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
+import Search from "../Search/Search";
+
 const Navbar = () => {
   return (
     <nav className={style.navbar}>
       <Logo />
+      <Search placeholder={"Search a album of your choice"} />
       <Button children={"Give Feedback"} />
     </nav>
   );
---
+++ qtify/src/components/Search/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Search.module.css";
+import { ReactComponent as SearchIcon } from "../../assets/Search icon.svg";
+
+const Search = () => {
+  return (
+    <form className={styles.wrapper}>
+      <input
+        className={styles.search}
+        placeholder="Search a album of your choice"
+      />
+      <button className={styles.searchBtn}>
+        <SearchIcon />
+      </button>
+    </form>
+  );
+};
+
+export default Search;
