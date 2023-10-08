91a52d133926c9a2f8613fe446cc4d3bd0bdf806
Abhinav Chandurkar <abhinavchandurkar55@gmail.com>
2023-10-08 13:21:41 +0530

    card-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import HeroSection from "./components/HeroSection/HeroSection";
 import Navbar from "./components/Navbar/Navbar";
+import Card from "./components/Card/Card";
 
 function App() {
+  //Mock data to show the card created
+
+  const data = {
+    image: "card-image.png",
+    follow: 100,
+    name: "New Bollywood",
+  };
+
   return (
     <div className="App">
       <Navbar />
       <HeroSection />
+      <Card data={data} />
     </div>
   );
 }
